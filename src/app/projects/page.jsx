90e14d186f1b2ca433d31c0374b20486355eebff
Daniyal <daniyal733khan@gmail.com>
2024-01-15 19:03:13 +0530

    1 bug fixed

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -13,7 +13,7 @@ age:19
 return(<>
 
     <h1 className="flex justify-center">This is Projects page</h1>
-    <li><a href="/projects/">Project 1</a></li>
+    <li><a href="/projects/1">Project 1</a></li>
     <li><a href="/projects/2">Project 2</a></li>
     <li><a href="/projects/3">Project 3</a></li>
     <li><a href="/projects/4">Project 4</a></li>
