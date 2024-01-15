28c9b6960bbbf3a3d63106828cdf2bff9799d9b2
Daniyal <daniyal733khan@gmail.com>
2024-01-15 16:26:50 +0530

    nothing new

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,7 @@
 "use client";
 import React from "react";
-import React, { useState } from "react";
 
 export default function Home() {
-  const [name, setName] =useState("Pathaan");
-
-  const apple = () => {
-    setName("jawan");
-  };
-
-  return (
-    <div>
-      <h1>This is updated name {name}</h1>
-      <button onClick={()=>apple()}>Click to update</button>
-    </div>
-  );
+ return(<h2>Home</h2>) 
+  
 }
