64f509d61fd8ed86f65213b744b9e879989871c5
Daniyal <daniyal733khan@gmail.com>
2024-01-16 17:21:21 +0530

    commit

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -15,7 +15,7 @@ return(<>
     <h1 className="flex justify-center">This is Projects page</h1>
     <li><a href="/projects/1">Project 1</a></li>
     <li><a href="/projects/2">Project 2</a></li>
-    <li><a href="/projects/3">Project 3</a></li>
+    <li><a href="/projects/2323">Project 3</a></li>
     <li><a href="/projects/4">Project 4</a></li>
     <h1>{data.name}</h1>
     
