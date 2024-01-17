852f315031a6e053efecece08955fd428cd873c6
Daniyal <daniyal733khan@gmail.com>
2024-01-17 17:06:09 +0530

    dynamic tested

--- src/app/projects/[...project]/page.jsx
+++ src/app/projects/[...project]/page.jsx
@@ -2,12 +2,13 @@
 import React from "react"
 
 export default function Project({params}) {
-
+console.log(params)
 return(<>
 
     <h1 className="flex justify-center">This is Projects page</h1>
-    <h2>This is project page no:{params.project}</h2>
-    
+    <h2>This is project page no:{params.project[0]}</h2>
+    <h2>This is project data no:{params.project[1]}</h2>
+
     </>)
 
 
